cb603079396d0f8474a0edb9f7e8698714369ab2
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-09-27 17:03:19 +0000

    set the server up

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+const express  = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const api = require('./api');
+
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 5002;
+
+app.get('/', (req, res) =>{
+    res.json({
+        message: 'Welcome to Mwaka Moon'
+    });
+});
+
+app.use(bodyParser.json());
+app.use('/api', api);
+
+
+app.listen(port,()=>{
+    console.log(`listening on http://localhost:${port}`)
+});
