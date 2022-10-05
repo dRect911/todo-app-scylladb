8a14c0680acdf55315fa7b2146c0960ea59eb50c
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-10-05 02:18:42 +0000

    responsivity and loading screen

--- client/src/components/AddItemForm.jsx
+++ client/src/components/AddItemForm.jsx
@@ -1,11 +1,39 @@
 import react, {useState, useEffect} from 'react';
 
 
-export default function AddItemForm(){
+export default function AddItemForm({onItemCreate}){
+    const [newItem, setNewItem] = useState('');
+
+    const onChange = (event) => {
+        setNewItem(event.target.value);
+    };
+    
+    const onCreate = (event) => {
+        event.preventDefault();
+        // send a POST request to API and 
+        // add the new item to the list
+        onItemCreate(newItem);
+        setNewItem('');
+    };
+
+
     return (
-        <div className="flex items-center ">
-            <input type="text" placeholder="Add a new item" className="w-[300px] h-8 border rounded-l bg-slate-100 border-slate-300 hover:border-slate-500 transition-all"></input>
-            <button type="submit" text="Add" className="w-[60px] h-8 bg-white text-sky-500 border border-sky-500 rounded-r hover:bg-sky-500 hover:text-white hover:border-sky-700 transition-all">Add</button>
-        </div>
+        <form onSubmit={onCreate} className="flex items-center mb-1 ">
+            <input 
+            type="text" 
+            placeholder="Add a new item" 
+            value={newItem}
+            onChange={onChange} 
+            className="w-[220px] 2xl:w-[740px] xl:w-[540px] lg:w-[440px] md:w-[340px] sm:w-[270px]
+            h-8 border rounded-l bg-slate-100 placeholder:text-slate-300 border-slate-400 hover:bg-gradient-to-r hover:from-slate-200 hover:to-slate-300  hover:placeholder:text-slate-500 focus:ring-opacity-0 focus:border-0 focus:transform-none focus:bg-gradient-to-r focus:from-slate-300 focus:to-slate-300 focus:placeholder:text-slate-500 transition-all">
+
+            </input>
+            <button 
+                type="submit" 
+                disabled={!newItem.length} 
+                className="w-[60px] h-8  enabled:hover:from-slate-400 enabled:hover:to-slate-400 enabled:hover:text-sky-100 border border-slate-400 rounded-r bg-gradient-to-r from-slate-300 to-slate-200 text-slate-500 disabled:hover:text-slate-300 disabled:hover:from-slate-100 disabled:hover:to-slate-100 transition-all">
+                Add
+            </button>
+        </form>
     )
 }
--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,21 +1,63 @@
-export default function Item(props){
-    const completed = props.completed
+import {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
+export default function Item({item, onItemDelete, onItemUpdate}){
+    const [completed, setCompleted] = useState(item.completed);
     // console.log(completed)
 
-    if (completed){
-        return(
-            <div key={props.id} className="w-[360px] h-8 p-1 border-b border-b-slate-200 hover:bg-slate-100 hover:border-b-slate-400 transition-all ">
-                <h3>{props.name}</h3>
-            </div>
-        )
-    }
-    else if (!completed){
-        return(
-            <div key={props.id} className="w-[360px] h-8 p-1 bg-stone-100 border-b border-b-stone-200 hover:bg-slate-100 hover:border-b-slate-400 transition-all ">
-                <h3 className="text-stone-400 " >{props.name}</h3>
-            </div>
-        )
+    const onDelete = () => {
+        onItemDelete(item)
     }
 
-    
+    const toggleCompletion = () => {
+        // handle completion here
+        // onItemUpdate(item);
+        setCompleted(!completed);
+    }
+
+
+    if (!completed){
+        return(
+            <div key={item.id} className="w-[280px] 2xl:w-[800px] xl:w-[600px] lg:w-[500px] md:w-[400px] sm:w-[330px]
+                h-8 p-1 flex justify-between items-center border-b border-b-slate-200 hover:bg-slate-100 hover:border-b-slate-400 transition-all ">
+                <div className="flex items-center">
+                    <input 
+                        type="checkbox" 
+                        checked={completed}
+                        onChange={toggleCompletion}
+                        className="ml-1 rounded checked:bg-stone-400 hover:checked:bg-stone-500 focus:accent-stone-500 hover:border-2 focus:ring-stone-500" >                    
+                    </input>
+                    <h3 className="ml-2" >{item.name}</h3>
+                </div>
+                
+                <FontAwesomeIcon onClick={onDelete} className="pr-2 opacity-0 hover:opacity-100 text-red-600 transition-all" icon={faXmark} />
+
+                {/* <h3 onClick={onDelete} className="opacity-0 hover:opacity-100 text-sky-500 pr-2 transition-all">Done</h3>
+                <input type="checkbox"></input> */}
+            </div>
+        )
+    }
+    else if (completed){
+        return(
+            <div key={item.id} className="w-[280px] 2xl:w-[800px] xl:w-[600px] lg:w-[500px] md:w-[400px] sm:w-[330px]
+             h-8 p-1 flex justify-between items-center bg-stone-100 border-b border-b-stone-200 hover:bg-slate-100 hover:border-b-slate-400 transition-all ">
+                <div className="flex items-center">
+                    <input 
+                        type="checkbox" 
+                        checked={completed}
+                        onChange={toggleCompletion}
+                        className="ml-1 rounded checked:bg-stone-400 hover:checked:bg-stone-500 focus:checked:bg-stone-500 hover:border-2 focus:ring-stone-500 " >                    
+                    </input>
+                    <h3 className="ml-2 text-stone-400 " >{item.name}</h3>
+                </div>
+                
+                
+
+                {/* <h3 className="text-red-600 pr-2">Done</h3> */}
+            </div>
+        )
+    }   
 }
+
+
